c3cc86d7ae79068f9af2e26be0782dd24d598d14
Thibaud Colas <thibaudcolas@gmail.com>
2016-05-18 13:07:06 +0200

    Read data from real CSS file

--- index.js
+++ index.js
@@ -6,24 +6,62 @@ var cssParse = require('css-parse');
 var unminify = require('cssbeautify');
 
 // https://github.com/pocketjoso/specificity-graph/blob/master/lib/generateCssData.js#L8
- function specSum(selector) {
-    var specs = specificity.calculate(selector)[0].specificity.split(',');
-    var sum = (parseInt(specs[0])*1000) + (parseInt(specs[1])*100) + (parseInt(specs[2])*10) + (parseInt(specs[3]));
-
-    return sum;
+var specSum = function(selector){
+  var specs = specificity.calculate(selector)[0].specificity.split(',');
+  var sum = (parseInt(specs[0])*1000) + (parseInt(specs[1])*100) + (parseInt(specs[2])*10) + (parseInt(specs[3]));
+  return sum;
 };
 
-var cssSelectorArray = [
-  'body { color: red;}',
-  '.div { color: red; }',
-  '.div.div { color: red; }',
-  'input[type=text] { color: red; }',
-  '#kill-me { color: red; }',
-  '@media screen and (min-width: 600px) { .media-query-selector { color: red; }',
-  '.another-one-inside-mq { color: red; } }'
-];
+/*
+ * Adds rule to results. Modifies pass in results array
+ * PARAMS: rule (object), selectorIndex (int), results (array)
+ * RETURNS: selectorIndex
+ */
+var addRuleToResults = function(rule, selectorIndex, results) {
+  var selectors = rule.selectors || [];
+  var line = rule.position.start.line;
 
-var result = cssSelectorArray.map(specSum);
-var sparkline = sparkly(result.concat(result).concat(result).concat(result).concat(result).concat(result), {style: 'fire'});
+  selectors.forEach(function(selector){
+    if(selector.length === 0) return;
+    results.push({
+      selectorIndex: selectorIndex,
+      line: line,
+      specificity: specSum(selector),
+      selectors: selector
+    });
+    selectorIndex++;
+  });
+  return selectorIndex;
+};
+
+var generateCssData = function(origCss) {
+
+  var testCSS = unminify(origCss);
+  var astObj = cssParse(testCSS, {silent: true});
+
+  var results = [],
+      selectorIndex = 0;
+
+  astObj.stylesheet.rules.map(function(rule){
+    if (rule.type === 'media') {
+      rule.rules.forEach(function(rule){
+        selectorIndex = addRuleToResults(rule, selectorIndex, results);
+      });
+    } else if (rule.type === 'rule') {
+      selectorIndex = addRuleToResults(rule, selectorIndex, results);
+    }
+
+  });
+
+  return results;
+
+};
+
+var fs = require('fs');
+var testStylesheet = fs.readFileSync('./examples/test.min.css', 'utf-8');
+
+var result = generateCssData(testStylesheet);
+var specificities = result.map(function(r) { return r.specificity; });
+var sparkline = sparkly(specificities, {style: 'fire', min: 0});
 
 console.log(sparkline);
