2b54b3f64bad4a116dddf8eee66deab47ef5cc35
Thibaud Colas <thibaudcolas@gmail.com>
2016-05-25 15:20:48 +0200

    Move height definition to chart rendering code

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ import { renderChart } from './lib/chart';
 
 export default (rawCSS) => {
   const isBig = false;
-  const CHART_HEIGHT = isBig ? 20 : 3;
 
   const result = generateCssData(rawCSS);
   const specificities = result.map(r => r.specificity);
 
-  return renderChart(CHART_HEIGHT, isBig, specificities);
+  return renderChart(isBig, specificities);
 };
--- tests/chart.spec.js
+++ tests/chart.spec.js
@@ -2,7 +2,7 @@ import { describe, it } from 'mocha';
 import { expect } from 'chai';
 
 describe('Chart', () => {
-    it('works', () => {
-        expect(true).to.equal(true);
-    });
+  it('works', () => {
+    expect(true).to.equal(true);
+  });
 });
